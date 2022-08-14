83a9fab3609b637117b7f7c34f841f12ec8b7705
Alex Guillu <alex@Alexs-MBP.oswgilaa.metronetinc.net>
2022-08-14 16:28:15 -0500

    completed all tasks in the module

--- index.js
+++ index.js
@@ -15,10 +15,32 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function (edible) {
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function () {
+  this.stomach = [];
+}
+
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+}
+
+// const jason = new Person('Jason', 27);
+// console.log(jason.toString());
+// jason.eat('tacos');
+// console.log(jason.stomach);
+// jason.poop();
+// console.log(jason.stomach);
 
 /*
   TASK 2
@@ -36,10 +58,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon =20;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function (gallons) {
+  this.tank = this.tank + gallons;
+}
 
 /*
   TASK 3
@@ -49,10 +77,15 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`;
+} 
 
 /* 
   TASK 4
